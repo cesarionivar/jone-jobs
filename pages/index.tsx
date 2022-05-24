3231682d52910fecd91e75e13d13d5cbc043b7fd
Cesario Nivar <ing.cesarionivar@gmail.com>
2022-05-24 00:33:12 -0400

    chore: add placeholder of mapbox container

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,7 +3,7 @@ import Head from 'next/head';
 
 const Home: NextPage = () => {
   return (
-    <div>
+    <div className='text-white'>
       <Head>
         <title>Jone Jobs</title>
         <meta name='description' content='Find j1  jobs easyly' />
@@ -11,6 +11,7 @@ const Home: NextPage = () => {
       </Head>
 
       <h1 className='text-3xl text-center mt-10 font-bold'>Jone Jobs</h1>
+      <div className='absolute top-0 left-0 w-full h-full bg-slate-800 -z-10' />
     </div>
   );
 };
